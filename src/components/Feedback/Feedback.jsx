52da8690016a258bb8357240284586c59b748369
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-08-21 16:37:07 +0300

    make-markup

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -5,25 +5,49 @@ export class Feedback extends Component {
         good: 0,
         neutral: 0,
         bad: 0
-      } 
-      render(){
+    } 
+    
+    handleClickGood = () => 
+        this.setState((prevState) => ({ good: prevState.good +=1}))     
+
+    handleClickNeutral = () => 
+        this.setState((prevState) => ({ neutral: prevState.neutral +=1}))    
+
+    handleClickBad = () => 
+        this.setState((prevState) =>({ bad: prevState.bad +=1}))    
+
+        countTotalFeedback = () => {
+            const { good, neutral, bad } = this.state;
+            const result = good + neutral + bad;
+            return result;
+        }
+        
+        countPositiveFeedbackPercentage = () => {
+            const { good, neutral, bad } = this.state;
+            const result = good + neutral + bad;
+            const percentGood =  good * 100 / result;
+            return Math.round(percentGood);
+        }
+
+    render(){
+        const { good, neutral, bad } = this.state;
         return(
         <div>
             <h1>Please leave feedback</h1>
             <div>
-                <button>Good</button>
-                <button>Neutral</button>
-                <button>Bad</button>
+                <button onClick={this.handleClickGood}>Good</button>
+                <button onClick={this.handleClickNeutral}>Neutral</button>
+                <button onClick={this.handleClickBad}>Bad</button>
             </div>
             <h2>Statistics</h2>
+            
             <ul>
-                <li>Good: {this.state.good}</li>
-                <li>Neutral: {this.state.neutral}</li>
-                <li>Bad: {this.state.bad}</li>
+                <li>Good: {good}</li>
+                <li>Neutral: {neutral}</li>
+                <li>Bad: {bad}</li>
+                <li>Total: {this.countTotalFeedback()}</li>
+                <li>Positive Feedback: { this.countPositiveFeedbackPercentage() }%</li>
             </ul>
-
-            <p>Total: 6</p>
-            <p>Positive Feedback: 50%</p>
         </div>
         
      ) }
