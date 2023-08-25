f59b32dc9f184d0d29c403d5be3b809cf23275d5
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-08-25 11:02:55 +0300

    save-transition

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -7,12 +7,14 @@ border-radius: 4px;
 margin-right:4px;
 padding:8px;
 color: white;
-background: gray;
+background-color: gray;
 cursor: pointer;
 
 border: 1.5px solid rgb(171, 163, 163);
 box-shadow: 0px 1px 6px rgba(46, 47, 66, 0.08),
     0px 1px 1px rgba(46, 47, 66, 0.16), 0px 2px 1px rgba(46, 47, 66, 0.08);
+
+    transition: color 250ms linear,background-color 250ms linear, box-shadow 250ms linear ;
     &:hover {
     background-color: white;
     color: black;
